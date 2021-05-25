5fcd99561ba5c14404389e00dddc4c15e48360af
robinwatts96 <robinwatts96@gmail.com>
2021-05-25 08:03:48 +0100

    Update

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
-const colors = {"green", "red", "rgba(133,122,200)", "#f15025";
-}
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025";
+];
 
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
 
-btn.addEventListener('click', function() {
-    
-})
+btn.addEventListener("click", function() {
+    // Get a number between between 0-3 (for the array)
+    const randomNumber = 2;
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors [randomNumber];
+});
 
